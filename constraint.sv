ab8927607f8ff53f443fd5e02312426d99276660
doppalapudiyasaswini-glitch <doppalapudiyasaswini@gmail.com>
2026-05-06 21:53:59 +0530

    Add files via upload
    generate  a constraint 2,4,8,16,32,128 (2power value)

--- constraint.sv
+++ constraint.sv
@@ -1,16 +1,16 @@
- class sample;
-rand int a[10];
+class sample;
+rand int a[];
 constraint a_c{
+a.size==10;
 foreach(a[i])
-  a[i]==(i+1)*-5;
+a[i]==2*(2**i);//it work
+a[i]==(2**i);// it is also work
 }
 endclass
 module top;
-sample s;
+sample s=new();
 initial begin
-s=new();
 s.randomize();
 $display("a=%p",s.a);
 end
 endmodule
-
